2c8d15bf97140cf1c015528045759437eecd128f
Omar Azeez <azeez.developer@outlook.com>
2025-12-25 21:58:47 -0500

    Re-install Vite for frontend

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0caa31f260efd365315a1822d089f410eccaff2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dd382abc2ca8ab321f20a870f313a0e577c254e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
